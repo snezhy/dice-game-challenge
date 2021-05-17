cce7afa9c99aeff54120b06a2ec408545f294d4f
Snezhana <snezhy@rocketmail.com>
2021-05-17 17:57:39 +0100

    adding game functionalities

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,100 @@
+const $form = $('form')
+const stop = document.querySelector(".btn-stop-rolling");
+const currentScorePlayerOne = document.querySelector("#current-score-player-one");
+const currentScorePlayerTwo = document.querySelector("#current-score-player-two");
+const scorePlayerOne = document.querySelector("#score-player-one");
+const scorePlayerTwo = document.querySelector("#score-player-two");
+const newGame = document.querySelector(".btn-new-game");
+const panelPlayerOne = document.querySelector(".player-one-panel");
+const panelPlayerTwo = document.querySelector(".player-two-panel");
+let counter = 0;
+let totalScorePlayerOne = 0
+let totalScorePlayerTwo = 0
 
-const btn = document.querySelector('button')
-btn.addEventListener('click', () => {
-    const diceOne = document.querySelector('#diceOne')
-    const diceTwo = document.querySelector('#diceTwo')
-    const diceOneRes = (diceOne.src).slice(-4, 1)
-    const diceTwoRes = (diceTwo.src).slice(-4, 1)
-    console.log('one' + diceOneRes)
-    console.log('two' + diceTwoRes)    
+console.log('$form ', $form)
+$form.on('submit', submitHandler)
+function submitHandler (e) {
+    e.preventDefault()
+
+    $.ajax({
+        url: '/game',
+        type:'GET'
+    }).done((response) => {
+        console.log(response)
+        diceOne.src =response.diceOne
+        diceTwo.src = response.diceTwo
+        const playerOneActive = response.playerOneActive
+        counter += response.playerOne.diceOneNum + response.playerOne.diceTwoNum
+        setActivePlayer(playerOneActive) 
+    })
+}
+
+stop.addEventListener("click", () => {
+  if (panelPlayerOne.classList.contains("active")) {
+    totalScorePlayerOne += counter
+    scorePlayerOne.textContent = totalScorePlayerOne
+    currentScorePlayerOne.textContent = 0
+    if (totalScorePlayerOne >= 100) {
+      alert("Player 1 Wins!")
+      resetGame()
+      return
+    }
+  } else {
+      totalScorePlayerTwo += counter
+      scorePlayerTwo.textContent = totalScorePlayerTwo
+      currentScorePlayerTwo.textContent = 0
+          if (totalScorePlayerTwo >= 100) {
+            alert("Player 2 Wins!")
+            resetGame()
+            return
+         }
+  }
+  counter = 0;
+  panelPlayerOne.classList.toggle("active")
+  panelPlayerTwo.classList.toggle("active")
 })
+
+newGame.addEventListener("click", () => {
+  resetGame();
+});
+
+function setActivePlayer(playerOneActive) {
+    if (playerOneActive) {
+        if (panelPlayerOne.classList.contains("active")) {
+            currentScorePlayerOne.textContent = counter;
+        } else {
+            currentScorePlayerTwo.textContent = counter;
+        }
+    } else {
+        counter = 0;
+        currentScorePlayerOne.textContent = 0;
+        currentScorePlayerTwo.textContent = 0;
+        panelPlayerOne.classList.toggle("active");
+        panelPlayerTwo.classList.toggle("active");
+    }
+}
+
+function rnd() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function resetGame() {
+  currentScorePlayerOne.textContent = 0;
+  currentScorePlayerTwo.textContent = 0;
+  scorePlayerOne.textContent = 0;
+  scorePlayerTwo.textContent = 0;
+  totalScorePlayerOne = 0;
+  totalScorePlayerTwo = 0;
+  counter = 0;
+  if (!panelPlayerOne.classList.contains("active")) {
+    panelPlayerOne.classList.toggle("active");
+    panelPlayerTwo.classList.toggle("active");
+  }
+}
+
+
+
+
 
 
 
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const path = require('path')
 const express = require('express')
 
 const Player = require('./player.js')
-const playerOne = new Player()
+const { Console } = require('console')
+const playerOne = new Player("Player 1")
+const playerTwo = new Player("Player 2")
 
-console.log(playerOne.rollDice())
+console.log(playerOne.isPlayerActive())
 const app = express()
 
 const publicDir = path.join(__dirname, '../public')
@@ -18,6 +20,21 @@ app.get('', (req, res) => {
     title: "This is a Dice Game",
     diceOne: "/img/dice"+playerOne.rollDice() + ".png",
     diceTwo: "/img/dice"+playerOne.rollDice() + ".png",
+    })
+})
+
+app.get('/game', (req, res) => {
+    playerOne.diceOneNum = playerOne.rollDice() 
+    playerOne.diceTwoNum = playerOne.rollDice() 
+    playerOne.calculateScore()
+    let playerOneActive = playerOne.isPlayerActive()
+    res.send({
+    title: "This is a Dice Game",
+    playerOneActive: playerOneActive,
+    diceOne: "/img/dice" + playerOne.diceOneNum + ".png",
+    diceTwo: "/img/dice" + playerOne.diceTwoNum + ".png",
+    playerOne:  playerOne,
+    playerTwo: playerTwo
     })
 })
 
